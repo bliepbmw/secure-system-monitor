78de06746cf308abf523d7fe80582637f61cbb54
bliepbmw <lukadejanovicstage@gmail.com>
2026-02-26 15:59:38 +0100

    Implement continuous daemon monitoring mode

--- main.lua
+++ main.lua
@@ -1,12 +1,19 @@
 local FileMonitor = require("src.file_monitor")
 local ProcessMonitor = require("src.process_monitor")
 local PortMonitor = require("src.port_monitor")
+local LogMonitor = require("src.log_monitor")
+local Baseline = require("src.baseline")
 local Logger = require("src.logger")
 
-Logger:log("INFO", "Starting Secure System Monitor")
+Logger:log("INFO", "Secure System Monitor started")
 
-FileMonitor:check()
-ProcessMonitor:check()
-PortMonitor:check()
+while true do
+    FileMonitor:check()
+    ProcessMonitor:check()
+    PortMonitor:check()
+    LogMonitor:check_auth()
+    Baseline:compare("/etc/passwd")
 
-Logger:log("INFO", "Monitoring cycle complete")
+    Logger:log("INFO", "Cycle complete\n")
+    os.execute("sleep 10")
+end
